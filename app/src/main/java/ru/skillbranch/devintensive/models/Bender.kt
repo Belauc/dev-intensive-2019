f8c4941af2f15cf817fb9c47eaece26e2e7c3c3d
Belauc <47719838+Belauc@users.noreply.github.com>
2020-03-10 01:34:45 +0300

    hometask_3

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -13,19 +13,15 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int,Int,Int>>{
-        val (answValid, check) = question.answerValidation(answer)
-        if (check)
-        {
+
             return if(question.answers.contains(answer)){
                 question = question.nextQuestion()
-                "Отлично - это правильный ответ \n ${question.question}" to status.color
+                "Отлично - это правильный ответ\n${question.question}" to status.color
             }else{
                 status = status.nextStatus()
-                "Это не правильный ответ \n ${question.question}" to status.color
+                "Это не правильный ответ\n${question.question}" to status.color
             }
-        }else{
-            return answValid + "\n ${question.question}" to status.color
-        }
+        
 
     }
     enum class Status(val color: Triple<Int,Int,Int> ) {
